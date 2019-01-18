e53ce955a98dd96c3e9637678c1cd22847e53bb1
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 13:33:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash
+def my_hash(obj)
   your_hash = { "my_name" => "Sruthi Krishna"
                 "age" => 28
                 "occupation" => "Student"
@@ -8,6 +8,7 @@ end
 
 
 def shipping_manifest
+  the_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
