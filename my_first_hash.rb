0292bd97c10e353e5313ef2465ddbd7945f0e0bb
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 13:41:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,7 +8,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whole bone corsets" => 5,
+  the_manifest = { "whale bone corsets" => 5,
                    "porcelain vases" => 2,
                    "oil paintings" => 3
   }
