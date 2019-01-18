a07b6a6d6bbf6b4e8ac81e1df0556d57912aa3b4
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 13:46:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,6 +37,8 @@ def adding
                  }
     
     shipping_manifest["muskets"] = 2
+    shipping_manifest["gun powder"] = 4
+    
   
   # add 2 muskets to the shipping_manifest hash below
 
